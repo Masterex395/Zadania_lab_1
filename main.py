5c7807ce8ff318e14d0a87c7b3d7792a5629f7cb
mgrzes <163082@student.uwm.edu.pl>
2021-02-25 11:14:48 +0100

    Zadania

---
+++ main.py
@@ -0,0 +1,75 @@
+# # Zad 1
+# liczba1 = 5
+# liczba2 = 99
+# liczba3 = 5.5
+# liczba4 = 6.456
+# napis1 = 'Tu jest napis'
+# napis2 = 'Tu jest napis drugi'
+#
+# print(liczba1)
+# print(liczba2)
+# print(liczba3)
+# print(liczba4)
+# print(napis1)
+# print(napis2)
+
+# # Zad 2
+# from math import *
+#
+# a = 4
+# b = 2
+#
+# dodawanie = a+b
+# odejmowanie = a-b
+# mnozenie = a*b
+# dzielenie = a/b
+# dzielenie_calkowite = a//b
+# reszta = a%b
+# potega = a**b
+# pierwiastek = (sqrt(a))
+#
+# print(dodawanie)
+# print(odejmowanie)
+# print(mnozenie)
+# print(dzielenie)
+# print(dzielenie_calkowite)
+# print(reszta)
+# print(potega)
+# print(pierwiastek)
+
+# # Zad 3
+# from math import *
+# a = 3
+# b = 5
+# c = 9
+# d = 10
+# e = 2
+# f = 4
+# print(a)
+# print(b)
+# print(c)
+# print(d)
+# print(e)
+# print(f)
+# a += 6
+# print(a)
+# b -= 3
+# print(b)
+# c *= 2
+# print(c)
+# d /= 2
+# print(d)
+# e **= 2
+# print(e)
+# f %= 1
+# print(f)
+
+# # Zad 4
+# from math import *
+# euler = e**10
+# print(euler)
+
+# from math import *
+# x=(log(5 + sin(8)**2))
+# a=pow(x,1/6)
+# print(a)
